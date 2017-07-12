49b118924991abf86852b5ad969cfe62d2f001fa
Brian Downing <bdowning@lavos.net>
2017-07-11 22:47:59 -0500

    Normalized and pretty json netlist

--- generate-netlist-json.js
+++ generate-netlist-json.js
@@ -3,16 +3,42 @@ const xlsx = require('xlsx');
 
 const wb = xlsx.readFile('signal netlist.ods');
 
+function mangleForSort(s) {
+    if (s === undefined)
+        return 'undefined';
+    return s.toString().replace(/\d+/g, m => ('00000'+m).substr(-5));
+}
+
+function compKey(keyFn) {
+    return (a, b) => keyFn(a) < keyFn(b) ? -1 : 1;
+}
+
+function doSheet(name) {
+    const sheet = wb.Sheets[name];
+    if (sheet) {
+        let json = xlsx.utils.sheet_to_json(sheet);
+        if (name === 'Components') {
+            json.forEach(comp => {
+                ['1H','2H','3H','4H','5H','6H','7H','8H','9H','10H',
+                 'key'].forEach(k => delete comp[k]);
+            });
+            json = json.filter(x => x.component != null);
+            json.sort(compKey(x => mangleForSort(`${x.subnet}/${x.component}`)));
+        }
+        return json;
+    }
+}
+
 const ob = {
-    comps: xlsx.utils.sheet_to_json(wb.Sheets['Components']),
-    towerWires: xlsx.utils.sheet_to_json(wb.Sheets['Tower wires']),
-    caseAWires: xlsx.utils.sheet_to_json(wb.Sheets['Case A wires']),
-    caseBWires: xlsx.utils.sheet_to_json(wb.Sheets['Case B wires']),
-    caseCWires: xlsx.utils.sheet_to_json(wb.Sheets['Case C wires']),
-    crossConnections: xlsx.utils.sheet_to_json(wb.Sheets['Cross-connections']),
-    levers: xlsx.utils.sheet_to_json(wb.Sheets['Tower levers']),
-    leverLocking: xlsx.utils.sheet_to_json(wb.Sheets['Lever locking']),
-    simulation: xlsx.utils.sheet_to_json(wb.Sheets['Simulation']),
+    comps: doSheet('Components'),
+    towerWires: doSheet('Tower wires'),
+    caseAWires: doSheet('Case A wires'),
+    caseBWires: doSheet('Case B wires'),
+    caseCWires: doSheet('Case C wires'),
+    crossConnections: doSheet('Cross-connections'),
+    levers: doSheet('Tower levers'),
+    leverLocking: doSheet('Lever locking'),
+    simulation: doSheet('Simulation'),
 };
 
-fs.writeFileSync('netlist.json', JSON.stringify(ob));
+fs.writeFileSync('netlist.json', JSON.stringify(ob, null, 2));
