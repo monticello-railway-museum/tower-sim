d49830e5424a0e210d928a81350ea52c8c2b74b7
Brian Downing <bdowning@lavos.net>
2017-08-03 21:22:17 -0500

    More fixes

--- index.js
+++ index.js
@@ -576,9 +576,9 @@ class Top extends React.Component {
 
         {
             let state = 'BAD ASPECT';
-            const d1 = sim.components['Case A/CASEA'].terminals['T75'].voltage() > 8;
-            const d2 = sim.components['Case A/CASEA'].terminals['T76'].voltage() > 8;
-            const d3 = sim.components['Case A/CASEA'].terminals['T77'].voltage() > 8;
+            const d1 = sim.components['Case A/CASEA'].terminals['T77'].voltage() > 8;
+            const d2 = sim.components['Case A/CASEA'].terminals['T78'].voltage() > 8;
+            const d3 = sim.components['Case A/CASEA'].terminals['T79'].voltage() > 8;
             if (!d1 && !d2 && !d3)
                 state = 'Off';
             if (d1 && d2 && !d3)
